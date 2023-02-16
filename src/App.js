4e4eba264373d3fe93fd28d722c03b279cb8e333
Santhosh302002 <l.santhoshkumar30@gmail.com>
2023-02-16 12:52:03 +0530

    #

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import logo from './logo.svg';
 // import './App.css';
 
 function App() {
-  fetch("https://complimentr.com/api").
-  then()
   return (
     <div className="App">
      
     </div>
   );
 }
-
 export default App;
